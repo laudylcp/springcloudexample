77c482e38eab2f6c7a0f21a3f983efaf598423d5
laudy <2673236705@qq.com>
2017-10-21 01:11:27 +0800

    add simple user service module

---
+++ simple-user-service/src/main/java/com/laudy/simpleuserservice/SimpleUserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.laudy.simpleuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleUserServiceApplication.class, args);
+	}
+}
