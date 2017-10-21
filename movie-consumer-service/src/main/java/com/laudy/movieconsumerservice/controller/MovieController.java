80f2920e4c7bf81c9b703dc67a23bead930c14c5
laudy <2673236705@qq.com>
2017-10-21 16:44:26 +0800

    enable eureka client health check

--- movie-consumer-service/src/main/java/com/laudy/movieconsumerservice/controller/MovieController.java
+++ movie-consumer-service/src/main/java/com/laudy/movieconsumerservice/controller/MovieController.java
@@ -2,10 +2,14 @@ package com.laudy.movieconsumerservice.controller;
 
 import com.laudy.movieconsumerservice.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -13,8 +17,17 @@ public class MovieController {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id){
         return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
     }
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("simple-user-service");
+    }
+
 }
