9085a35dd4a7f0eab84239c7dd13e41fef40f425
laudy <2673236705@qq.com>
2017-10-21 15:23:53 +0800

    add eureka server module

--- simple-user-service/src/main/java/com/laudy/simpleuserservice/SimpleUserServiceApplication.java
+++ simple-user-service/src/main/java/com/laudy/simpleuserservice/SimpleUserServiceApplication.java
@@ -2,7 +2,9 @@ package com.laudy.simpleuserservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class SimpleUserServiceApplication {
 
