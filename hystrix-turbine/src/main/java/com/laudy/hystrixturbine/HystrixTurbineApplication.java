4195e1812f7617285c6b808de2b92eeb3bd141df
laudy <2673236705@qq.com>
2017-10-22 15:22:53 +0800

    add hystrix rabbitmq stream

--- hystrix-turbine/src/main/java/com/laudy/hystrixturbine/HystrixTurbineApplication.java
+++ hystrix-turbine/src/main/java/com/laudy/hystrixturbine/HystrixTurbineApplication.java
@@ -2,9 +2,9 @@ package com.laudy.hystrixturbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-@EnableTurbine
+@EnableTurbineStream
 @SpringBootApplication
 public class HystrixTurbineApplication {
 
